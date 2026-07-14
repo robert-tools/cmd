4c415d9513e597a4fd62ed041b53e6e0dc374e06
Robert Willemelis <robert.dev@gmx.de>
2026-07-14 19:28:29 +0200

    feat: add basic command tooling

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,32 @@
-import { sample } from './index';
+/**
+ * @jest-environment node
+ */
+import { LOG } from '@robert.tools/log';
+import * as cmd from './index';
 
-describe('@robert.tools/sample', () => {
-    it('should return a sample string', () => {
-        expect(sample('hello')).toBe('sample: hello');
+describe('cmd', () => {
+    it('should return a valid command', () => {
+        const command = 'echo "Hello, World!"';
+        expect(cmd.command(command)).toEqual('Hello, World!\n');
     });
-
-    it('should return a sample string with empty input', () => {
-        expect(sample('')).toBe('sample: ');
+    it('should return a valid command', () => {
+        const command = 'pwd .';
+        const result = cmd.command(command);
+        const pwd = process.cwd();
+        expect(result).toEqual(pwd + '\n');
+    });
+    it('should return an empty output when error occurs', () => {
+        const logSpy = jest.spyOn(LOG, 'FAIL');
+        const command = 'abcdefghijklmnopqrstuvwxyz';
+        expect(cmd.command(command, true, true)).toEqual('');
+        expect(logSpy).toHaveBeenCalledWith(
+            expect.stringContaining('Command failed')
+        );
+        logSpy.mockRestore();
+    });
+    it('should return a valid command with error', () => {
+        const command = 'echo "Hello, World!" && exit 1';
+        const result = cmd.command(command, true, false);
+        expect(result).toEqual('');
     });
 });
